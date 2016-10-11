6dd623ba83429bed539945961938657f2dd61031
rajrin@yahoo.com <rajrin@yahoo.com>
2016-10-10 22:14:17 -0400

    initial

---
+++ api/v1/resource.js
@@ -0,0 +1,78 @@
+/**
+ * Each resource resides in its own module.
+ * This template may be used for creating the API for Resources & Non-Resources
+ * 
+ * References:
+ *      https://expressjs.com/en/guide/routing.html
+ */
+
+// As a best practice keep the resource name same as the file name
+var RESOURCE_NAME = 'resource';
+var VERSION = 'v1';
+var URI = '/' + VERSION + '/' + RESOURCE_NAME; 
+
+module.exports = function(router){
+    'use strict';
+
+    // RETRIEVE Collection = GET
+    router.route(URI).get(function(req, res,next){
+        res.send('hello get collection')
+    });
+
+    // RETRIEVE Specific Resource = GET with id
+    // If the value of :id='search' then let it pass to the next handler
+    router.route(URI+'/:id').get(function(req, res,next){
+        if(req.params.id === 'search') next();
+        else 
+            res.send('hello get with id='+req.params.id)
+    });
+
+    // RETRIEVE Collection with QUERY Parameters = GET with ?
+    // URI would be    /v1/resource/search
+    router.route(URI+'/search').get(function(req, res,next){
+        res.send('hello get query with q='+ req.query['q'])
+    });
+
+    // CREATE = POST
+    router.route(URI).post(function(req,res,next){
+        if(!isDocumentValid(req.body)){
+            res.send('Document create request failed')
+        } else {
+            res.send('Document create request success');
+        }
+    });
+
+    // UPDATE = PUT
+     router.route(URI).put(function(req,res,next){
+        if(!isDocumentValid(req.body)){
+            res.send('Document update request failed')
+        } else {
+            res.send('Document update request success');
+        }
+    });
+
+    // DELETE = DELETE
+     router.route(URI).delete(function(req,res,next){
+        if(!isDeleteOk(req.body)){
+            res.send('Document delete request failed')
+        } else {
+            res.send('Document delete request success');
+        }
+    });
+   
+
+    // VALIDATION of document - specific to the resource
+    var isDocumentValid = function(doc){
+        // provid the logic for VALIDATION
+        
+        return doc.success;
+    }
+
+    // IS DELETE OK - check if the request for deleting the document is OK
+    var isDeleteOk = function(doc){
+        // provide the logic for checking
+
+        return doc.success;
+    }
+
+}
---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Main file 
+ * 
+ * References:
+ *      https://expressjs.com/en/4x/api.html
+ * 
+ */
+var express = require('express')
+var bodyParser = require('body-parser')
+
+// Setup the Router
+// For each resource follow the pattern for registring the resource API
+// ADD NEW RESOURCE API UNDER THIS
+var router = express.Router();
+require('./api/v1/resource')(router);
+
+// Create the express app
+app = express();
+// Setup the body parser
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());//{type: '*/*'}));
+
+// Setup the app to use the router
+app.use(router);
+
+// Start the listener
+app.listen(3000);
+console.log('Listening on 3000')
+
+
+
